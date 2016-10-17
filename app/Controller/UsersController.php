6e0b1d91584f56aaf1b8328ab32e4f2982628d9a
moddipen <moddipen@gmail.com>
2016-10-17 16:50:27 +0530

    no message

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -477,7 +477,7 @@ public function user_reset($token=null)
 	{
 		$this->layout = 'dashboard';
 		$this->set("title","Profile Page");
-		
+		echo "<pre>";print_r($this->Auth->user);exit;
 		$user = $this->User->find('first',array('conditions'=>array('User.id'=>$this->Auth->user('id'))));
 		$this->set("User",$user);
 
