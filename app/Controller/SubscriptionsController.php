535ed5bdfbcecf23a5c62bf9ad0ed913e87663a9
moddipen <moddipen@gmail.com>
2016-11-14 15:51:06 +0530

    no message

--- app/Controller/SubscriptionsController.php
+++ app/Controller/SubscriptionsController.php
@@ -249,9 +249,9 @@ class  SubscriptionsController  extends AppController {
 						$days = $diff->format('%a');//Day plan used
 						$amount_to_refund = $amount_to_refund * $days;
 						$amount_to_refund = $order['Order']['price'] - $amount_to_refund; // Total refunded amount
-						$amount_to_refund = $amount_to_refund/3;
-						$refund = Braintree_Transaction::refund($order['Order']['transiction_id'],31.11);
-						echo "<pre>";print_r($refund);exit;
+						$amount_to_refund = round($amount_to_refund/3);
+						$refund = Braintree_Transaction::refund($order['Order']['transiction_id'],$amount_to_refund);
+						//echo "<pre>";print_r($refund);exit;
 						if($refund->success)
 						{
 							$refund = Braintree_Transaction::refund($order['Order']['transiction_id'],$amount_to_refund);
