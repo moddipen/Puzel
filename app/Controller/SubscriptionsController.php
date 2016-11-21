3e50858ca501836003c07c99289e440f3c619e89
moddipen <moddipen@gmail.com>
2016-11-21 18:13:18 +0530

    no message

--- app/Controller/SubscriptionsController.php
+++ app/Controller/SubscriptionsController.php
@@ -242,6 +242,7 @@ class  SubscriptionsController  extends AppController {
 					}
 					else
 					{
+						exit("hello");
 						//Fetch total payment and deduct acording to month and refund
 						
 						$amount_to_refund = $order['Order']['price'] / 30;
