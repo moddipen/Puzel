18a9e9624b3fd9a43159936434c105a39d3978c5
moddipen <moddipen@gmail.com>
2016-11-22 16:29:24 +0530

    no message

--- app/Controller/PuzzlesController.php
+++ app/Controller/PuzzlesController.php
@@ -76,7 +76,7 @@ class  PuzzlesController  extends AppController {
 		$list = $this->Puzzle->find('all',array('conditions'=>array('Puzzle.user_id'=>$this->Auth->user('id')),'fields'=>array('Puzzle.id','Puzzle.name')));
 		$this->set('Name',$list);
 
-		$status = $this->UserSubscription->find('first',array('conditions'=>array('UserSubscription.user_id'=>$this->Auth->user('id')),'Limit'=>1));
+		$status = $this->UserSubscription->find('first',array('conditions'=>array('UserSubscription.user_id'=>$this->Auth->user('id')),'Limit'=>1,'order'=>'UserSubscription.created Desc'));
 		$this->set('Userpieces',$status);
 	}	
 
