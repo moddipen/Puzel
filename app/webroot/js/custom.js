1582243fa46a32fa6b86c44113a3638e4e884323
moddipen <moddipen@gmail.com>
2016-12-09 21:18:02 +0530

    no message

--- app/webroot/js/custom.js
+++ app/webroot/js/custom.js
@@ -1,221 +1,54 @@
 $(document).ready(function()
 {
     
-	$(document).on('click', "#puzelacount", function(e) 
+  $(document).on('click', "#puzelacount", function(e) 
       {
         $("#signwithpuzzleaccount").val(1);
       });
       
-	  $(document).on('click', "#normalsign", function(e) 
+    $(document).on('click', "#normalsign", function(e) 
       {
-        $("#enrollwithpuzzleaccount").val(2);
+        $("#signwithpuzzleaccount").val(0);
       }); 
-	
-	$('head').append('<link rel="stylesheet" type="text/css" href="http://puzel.stage.n-framescorp.com/app/webroot/css/animations.css">');
-	var snipest_id =$('.snipest').attr('id');
-	var puzzle_id = snipest_id.replace("puzzle_", "");
-	
-	$.ajax
-	  ({
-		 type: "POST",
-		 url: "http://puzel.stage.n-framescorp.com/visitors/snipestimage/"+puzzle_id,
-		 dataType: 'text', 
-		 success:function(data)
-		 {
-			$('#'+snipest_id).html(data);
-		 }
-	});
-	
-	// $(document).on('submit', "#Imagedata", function(e) 
-	// {
- //         if($("#fname").val() == '')
- //         {
- //            $("#firsname").after("<p>Please enter first name. </p>")
- //            return false;
- //         } 
- //         else if($("#lname").val() == '')
- //         {
- //            $("#laname").after("<p>Please enter last name. </p>")
- //            return false;
- //         }
- //         else if($("#useremail").val() == '')
- //         {
- //            // var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- //            $("#useemail").after("<p>Please enter an email. </p>")
- //            return false;
-
- //         } 
- //         else 
- //         {
-
- //            var url = "http://puzel.stage.n-framescorp.com/visitors/process"; 
- //             // Form Submit Ajax  
- //              $.ajax({
- //                       type: "POST",
- //                       url: url,
- //                       data: $("#Imagedata").serialize(), // serializes the form's elements.
- //                       dataType: 'json', 
- //                       success: function(data)
- //                       {
-	// 					   if(data.message != "That email address has already taken. Please use another email." || data.Message == "OK")
- //                          {
- //                            $.ajax
- //                              ({
- //                                 type: "POST",
- //                                 url: "http://puzel.stage.n-framescorp.com/visitors/fetchimage/"+data.ImageId,
- //                                 dataType: 'text', 
- //                                 success:function(data)
- //                                 {
- //                                    var transition = $("#transition").val();
-									
-	// 								var obj = $.parseJSON(data);
-	// 								obj = obj.name;
-	// 								objs = obj.split('.');
-	// 								var get_name = objs[0].split('_');
-	// 								$('.'+objs[0]).css("background-image","url('http://puzel.stage.n-framescorp.com/img/puzzel/"+get_name[0]+"/"+obj+"')");  //background:url('<?php echo $this->webroot;?>'img/puzzel/"+objs[0]+"/"+obj+"')");
-									
-	// 								if(transition  == "Newspaper"){var classes = 'pt-page-rotateOutNewspaper pt-page-rotateInNewspaper pt-page-delay500';}
-	// 								if(transition  == "Cube to left"){var classes = 'pt-page-rotateCubeLeftOut pt-page-ontop pt-page-rotateCubeLeftIn';}
-	// 								if(transition  == "Cube to right"){var classes = 'pt-page-rotateCubeRightOut pt-page-ontop pt-page-rotateCubeRightIn';}
-	// 								if(transition  == "Cube to top"){var classes = 'pt-page-rotateCubeTopOut pt-page-ontop pt-page-rotateCubeTopIn';}
-	// 								if(transition  == "Cube to bottom"){var classes = 'pt-page-rotateCubeBottomOut pt-page-ontop pt-page-rotateCubeBottomIn';}
-	// 								if(transition  == "Flip right"){var classes = 'pt-page-flipOutRight pt-page-flipInLeft pt-page-delay500';}
-	// 								if(transition  == "Flip left"){var classes = 'pt-page-flipOutLeft pt-page-flipInRight pt-page-delay500';}
-	// 								if(transition  == "Flip top"){var classes = 'pt-page-flipOutTop pt-page-flipInBottom pt-page-delay500';}
-	// 								if(transition  == "Flip bottom"){var classes = 'pt-page-flipOutBottom pt-page-flipInTop pt-page-delay500';}
-									
-	// 								$('.'+objs[0]).addClass(classes);
-	// 								// $("#puzzle").html(data);
- //                                    $("#Imagedata")[0].reset();
- //                                    $("#success").html("<p style='font-size:14px;padding:20px'> You have been enrolled.</p>");
- //                                    $("#success").show().delay(3000).fadeOut(function(){ $(this).remove(); });
- //                                 }
- //                            });
- //                          }
- //                          else
- //                          {
- //                            $("#alert").html("<p style='font-size:14px;padding:20px;margin-bottom:10px;'>"+data.message+"</p>");
- //                            $("p").show().delay(3000).fadeOut(function(){ $(this).remove(); });
- //                          }
- //                          //$("#Imagedata")[0].reset();  
- //                       }
- //                     });
-
- //                e.preventDefault(); // avoid to execute the actual submit of the form.
- //            } 
- //          });
-	  
-
-      
-      // Check email is valid or not 
-      function validateEmail($email)
-      {
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        return emailReg.test( $email );
-      }
-
-      $(document).on('submit', "#Imageenroll", function(e) 
-      {
-
-         if($("#password").val() == '')
-         {
-            return false;
-         } 
-         else if( !validateEmail($("#userenrollemail").val()) || $("#userenrollemail").val() == '') 
-         {
-            return false;
-         } 
-         else 
-         {
-
-            var url = "<?php echo Configure::read('SITE_URL')?>visitors/process/<?php echo $explode[5];?>"; 
-             // Form Submit Ajax  
-              $.ajax({
-                       type: "POST",
-                       url: url,
-                       data: $("#Imageenroll").serialize(), // serializes the form's elements.
-                       dataType: 'json', 
-                       success: function(data)
-                       {
-
-                        if(data.message == "success" )
-                          {
-                            $.ajax
-                              ({
-                                 type: "POST",
-                                 url: "<?php echo Configure::read('SITE_URL');?>visitors/fetchimage/"+data.ImageId,
-                                 dataType: 'text', 
-                                 success:function(data)
-                                 {
-                                    var obj = $.parseJSON(data);
-                                    obj = obj.name;
-                                    objs = obj.split('.');
-                                    var get_name = objs[0].split('_');
-                                    $('.'+objs[0]).css("background-image","url('<?php echo $this->webroot;?>img/puzzel/"+get_name[0]+"/"+obj+"')");  //background:url('<?php echo $this->webroot;?>'img/puzzel/"+objs[0]+"/"+obj+"')");
-                                    
-                                    if(transition  == "Newspaper"){var classes = 'pt-page-rotateOutNewspaper pt-page-rotateInNewspaper pt-page-delay500';}
-                                    if(transition  == "Cube to left"){var classes = 'pt-page-rotateCubeLeftOut pt-page-ontop pt-page-rotateCubeLeftIn';}
-                                    if(transition  == "Cube to right"){var classes = 'pt-page-rotateCubeRightOut pt-page-ontop pt-page-rotateCubeRightIn';}
-                                    if(transition  == "Cube to top"){var classes = 'pt-page-rotateCubeTopOut pt-page-ontop pt-page-rotateCubeTopIn';}
-                                    if(transition  == "Cube to bottom"){var classes = 'pt-page-rotateCubeBottomOut pt-page-ontop pt-page-rotateCubeBottomIn';}
-                                    if(transition  == "Flip right"){var classes = 'pt-page-flipOutRight pt-page-flipInLeft pt-page-delay500';}
-                                    if(transition  == "Flip left"){var classes = 'pt-page-flipOutLeft pt-page-flipInRight pt-page-delay500';}
-                                    if(transition  == "Flip top"){var classes = 'pt-page-flipOutTop pt-page-flipInBottom pt-page-delay500';}
-                                    if(transition  == "Flip bottom"){var classes = 'pt-page-flipOutBottom pt-page-flipInTop pt-page-delay500';}
-                                    
-                                    $('.'+objs[0]).addClass(classes);
-                                    // $("#puzzle").html(data);
-                                    $("#Imageenroll")[0].reset();
-                                    // $("#success").html("<div style='background:rgba(60,118,61,0.5);color:#3C763D;font-size:14px;padding:20px'> Register successfully.</div>");
-                                    // $("#success").show().delay(3000).fadeOut(function(){ $(this).remove(); });
-                                    $("#alert").html("<p style='font-size:14px;'>Puzzle created successfully.</p>");
-                                    $("p").show().delay(3000).fadeOut(function(){ $(this).remove(); });
-                                   }
-                              });
-                          }
-                          else
-                          {
-                            $("#alert").html("<p style='font-size:14px;'>"+data.message+"</p>");
-                            $("p").show().delay(3000).fadeOut(function(){ $(this).remove(); });
-                          }
-                       }
-                     });
-
-                e.preventDefault(); // avoid to execute the actual submit of the form.
-            } 
-          });  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      $(document).on('submit', "#Imagedata", function(e) 
-      {
+  
+  $('head').append('<link rel="stylesheet" type="text/css" href="http://puzel.stage.n-framescorp.com/app/webroot/css/animations.css">');
+  var snipest_id =$('.snipest').attr('id');
+  var puzzle_id = snipest_id.replace("puzzle_", "");
+  
+  $.ajax
+    ({
+     type: "POST",
+     url: "http://puzel.stage.n-framescorp.com/visitors/snipestimage/"+puzzle_id,
+     dataType: 'text', 
+     success:function(data)
+     {
+      $('#'+snipest_id).html(data);
+     }
+  });
+  
+  $(document).on('submit', "#Imagedata", function(e) 
+  {
          if($("#fname").val() == '')
          {
+            $("#firsname").after("<p>Please enter first name. </p>")
             return false;
          } 
          else if($("#lname").val() == '')
          {
+            $("#laname").after("<p>Please enter last name. </p>")
             return false;
          }
-         else if( !validateEmail($("#useremail").val()) || $("#useremail").val() == '') 
+         else if($("#useremail").val() == '')
          {
+            // var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            $("#useemail").after("<p>Please enter an email. </p>")
             return false;
+
          } 
          else 
          {
 
-            var url = "<?php echo Configure::read('SITE_URL')?>visitors/process/<?php echo $explode[5];?>"; 
+            var url = "http://puzel.stage.n-framescorp.com/visitors/process"; 
              // Form Submit Ajax  
               $.ajax({
                        type: "POST",
@@ -224,43 +57,44 @@ $(document).ready(function()
                        dataType: 'json', 
                        success: function(data)
                        {
-                         if(data.message != "You have already enrolled")
+               if(data.message != "That email address has already taken. Please use another email." || data.Message == "OK")
                           {
                             $.ajax
                               ({
                                  type: "POST",
-                                 url: "<?php echo Configure::read('SITE_URL');?>visitors/fetchimage/"+data.ImageId,
+                                 url: "http://puzel.stage.n-framescorp.com/visitors/fetchimage/"+data.ImageId,
                                  dataType: 'text', 
                                  success:function(data)
                                  {
-                                    var obj = $.parseJSON(data);
-                                    obj = obj.name;
-                                    objs = obj.split('.');
-                                    var get_name = objs[0].split('_');
-                                    $('.'+objs[0]).css("background-image","url('<?php echo $this->webroot;?>img/puzzel/"+get_name[0]+"/"+obj+"')");  //background:url('<?php echo $this->webroot;?>'img/puzzel/"+objs[0]+"/"+obj+"')");
-                                    
-                                    if(transition  == "Newspaper"){var classes = 'pt-page-rotateOutNewspaper pt-page-rotateInNewspaper pt-page-delay500';}
-                                    if(transition  == "Cube to left"){var classes = 'pt-page-rotateCubeLeftOut pt-page-ontop pt-page-rotateCubeLeftIn';}
-                                    if(transition  == "Cube to right"){var classes = 'pt-page-rotateCubeRightOut pt-page-ontop pt-page-rotateCubeRightIn';}
-                                    if(transition  == "Cube to top"){var classes = 'pt-page-rotateCubeTopOut pt-page-ontop pt-page-rotateCubeTopIn';}
-                                    if(transition  == "Cube to bottom"){var classes = 'pt-page-rotateCubeBottomOut pt-page-ontop pt-page-rotateCubeBottomIn';}
-                                    if(transition  == "Flip right"){var classes = 'pt-page-flipOutRight pt-page-flipInLeft pt-page-delay500';}
-                                    if(transition  == "Flip left"){var classes = 'pt-page-flipOutLeft pt-page-flipInRight pt-page-delay500';}
-                                    if(transition  == "Flip top"){var classes = 'pt-page-flipOutTop pt-page-flipInBottom pt-page-delay500';}
-                                    if(transition  == "Flip bottom"){var classes = 'pt-page-flipOutBottom pt-page-flipInTop pt-page-delay500';}
-                                    
-                                    $('.'+objs[0]).addClass(classes);
+                                    var transition = $("#transition").val();
+                  
+                  var obj = $.parseJSON(data);
+                  obj = obj.name;
+                  objs = obj.split('.');
+                  var get_name = objs[0].split('_');
+                  $('.'+objs[0]).css("background-image","url('http://puzel.stage.n-framescorp.com/img/puzzel/"+get_name[0]+"/"+obj+"')");  //background:url('<?php echo $this->webroot;?>'img/puzzel/"+objs[0]+"/"+obj+"')");
+                  
+                  if(transition  == "Newspaper"){var classes = 'pt-page-rotateOutNewspaper pt-page-rotateInNewspaper pt-page-delay500';}
+                  if(transition  == "Cube to left"){var classes = 'pt-page-rotateCubeLeftOut pt-page-ontop pt-page-rotateCubeLeftIn';}
+                  if(transition  == "Cube to right"){var classes = 'pt-page-rotateCubeRightOut pt-page-ontop pt-page-rotateCubeRightIn';}
+                  if(transition  == "Cube to top"){var classes = 'pt-page-rotateCubeTopOut pt-page-ontop pt-page-rotateCubeTopIn';}
+                  if(transition  == "Cube to bottom"){var classes = 'pt-page-rotateCubeBottomOut pt-page-ontop pt-page-rotateCubeBottomIn';}
+                  if(transition  == "Flip right"){var classes = 'pt-page-flipOutRight pt-page-flipInLeft pt-page-delay500';}
+                  if(transition  == "Flip left"){var classes = 'pt-page-flipOutLeft pt-page-flipInRight pt-page-delay500';}
+                  if(transition  == "Flip top"){var classes = 'pt-page-flipOutTop pt-page-flipInBottom pt-page-delay500';}
+                  if(transition  == "Flip bottom"){var classes = 'pt-page-flipOutBottom pt-page-flipInTop pt-page-delay500';}
+                  
+                  $('.'+objs[0]).addClass(classes);
+                  // $("#puzzle").html(data);
                                     $("#Imagedata")[0].reset();
-                                    // $("#success").html("<div style='background:rgba(60,118,61,0.5);color:#3C763D;font-size:14px;padding:20px'> Register successfully.</div>");
-                                    // $("#success").show().delay(3000).fadeOut(function(){ $(this).remove(); });
-                                    $("#alert").html("<p style='font-size:14px;'>You have been enrolled.</p>");
-                                    $("p").show().delay(3000).fadeOut(function(){ $(this).remove(); });
-                                  }
+                                    $("#success").html("<p style='font-size:14px;padding:20px'> You have been enrolled.</p>");
+                                    $("#success").show().delay(3000).fadeOut(function(){ $(this).remove(); });
+                                 }
                             });
                           }
                           else
                           {
-                            $("#alert").html("<p style='font-size:14px;'>"+data.message+"</p>");
+                            $("#alert").html("<p style='font-size:14px;padding:20px;margin-bottom:10px;'>"+data.message+"</p>");
                             $("p").show().delay(3000).fadeOut(function(){ $(this).remove(); });
                           }
                           //$("#Imagedata")[0].reset();  
@@ -269,5 +103,6 @@ $(document).ready(function()
 
                 e.preventDefault(); // avoid to execute the actual submit of the form.
             } 
-          });  
+          });
+  
 }); 
