5e3d101e80e92753c6131ac670d14d7beb920150
moddipen <moddipen@gmail.com>
2016-12-13 12:41:13 +0530

    no message

--- app/Controller/SubscriptionsController.php
+++ app/Controller/SubscriptionsController.php
@@ -388,8 +388,8 @@ class  SubscriptionsController  extends AppController {
 									$tomorrow->setTime(0,0,0);	
 									$result = Braintree_Subscription::create([
 									  'paymentMethodToken' => $customer->customer->paymentMethods[0]->token,
-									  'planId' => $plan['Subscription']['id'],
-									   'firstBillingDate' => $tomorrow
+									  'planId' => $plan['Subscription']['id']
+									   // 'firstBillingDate' => $tomorrow
 									]);
 									// if($plan['Subscription']['id'] != 2)
 									// {
