2f0c7d50ba74f65bb6d6908fb16be930605f01ea
moddipen <moddipen@gmail.com>
2016-12-20 10:45:43 +0530

    no message

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1110,7 +1110,7 @@ public function user_reset($token=null)
 	{
 		$data =  $this->User->find('all',array('conditions'=>array('User.usertype'=>1),'order'=>'User.firstname Asc','fields'=>array('User.firstname','User.lastname','User.email','User.website','User.usertype','User.company_name','User.tokenhash','UserSubscription.used_pieces','UserSubscription.subscription_id','UserSubscription.id')));	
 	}	
-	echo "<pre>";print_r($data);exit;
+	
 	$index = 0;
 		foreach($data as $business)
 		{
