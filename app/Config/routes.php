b2c9adec604386f09ab71fb1b5a8d122e54451c4
moddipen <moddipen@gmail.com>
2016-12-21 13:35:50 +0530

    no message

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -112,7 +112,7 @@
   
 
   // refrel hosting page 
-  if(isset($explode[5]))
+  if(isset($explode[6]))
   {
         Router::connect(
             '/puzzle/:company_name/:name/:refrel', // E.g. /blog/3-CakePHP_Rocks
