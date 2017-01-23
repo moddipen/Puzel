d11dba41d0ecdf6551a8a6360732efeeabf0c26d
moddipen <moddipen@gmail.com>
2017-01-23 13:06:11 +0530

    no message

--- app/webroot/js/custom.js
+++ app/webroot/js/custom.js
@@ -69,11 +69,11 @@ $(document).ready(function()
                                     var transition = $("#transition").val();
                   
                                     var obj = $.parseJSON(data);
+
+                                    alert(obj);
                                     obj = obj.name;
                                     objs = obj.split('.');
                                     var get_name = objs[0].split('_');
-                                    console.log(get_name);
-                                    console.log(obj);
                                     $('.'+objs[0]).css("background-image","url('http://puzel.stage.n-framescorp.com/img/puzzel/"+get_name[0]+"/"+obj+"')");  //background:url('<?php echo $this->webroot;?>'img/puzzel/"+objs[0]+"/"+obj+"')");
                                     
                                     if(transition  == "Newspaper"){var classes = 'pt-page-rotateOutNewspaper pt-page-rotateInNewspaper pt-page-delay500';}
